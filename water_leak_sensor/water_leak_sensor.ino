29587a41e74d66b9eccc5be98775f24ffbb97256
BertSa <samuel.b.prog@gmai.com>
2021-09-08 20:33:07 -0400

    adding rf signal instead of led

--- water_leak_sensor/water_leak_sensor.ino
+++ water_leak_sensor/water_leak_sensor.ino
@@ -1,3 +1,6 @@
+#include <RCSwitch.h>
+RCSwitch mySwitch = RCSwitch();
+
 const byte buzzer1 = 0;
 const byte buzzer2 = 1;
 const byte statLED = 4;
@@ -13,9 +16,11 @@ void setup()
   pinMode(buzzer2, OUTPUT);
   pinMode(statLED, OUTPUT);
 
-  //pinMode(waterSensor, INPUT_PULLUP);
   pinMode(2, INPUT); //When setting the pin mode we have to use 2 instead of A1
   digitalWrite(2, HIGH); //Hack for getting around INPUT_PULLUP
+  mySwitch.setProtocol(1);
+  mySwitch.setPulseLength(300);
+  mySwitch.enableTransmit(3);
   waterAvg = 0;
   for (int x = 0 ; x < 8 ; x++)
   {
@@ -41,23 +46,23 @@ void loop()
   if (waterDifference > maxDifference)
   {
     long startTime = millis();
-    long timeSinceBlink = millis();
+    long timeSinceSignalSend = millis();
 
-    while (waterDifference > maxDifference || (millis() - startTime) < 2000)
+    do
     {
       if ((millis() % 2000 == 0))
         buzz();
-      if (millis() - timeSinceBlink > 5000)
+      if (millis() - timeSinceSignalSend > 5000)
       {
-        timeSinceBlink = millis();
+        timeSinceSignalSend = millis();
         digitalWrite(statLED, HIGH);
-        delay(500);
+        mySwitch.send(5592334, 24);
         digitalWrite(statLED, LOW);
       }
       waterDifference = abs(analogRead(waterSensor) - waterAvg);
-    }
+    } while (waterDifference > maxDifference || (millis() - startTime) < 2000)
 
-    digitalWrite(buzzer1, LOW);
+      digitalWrite(buzzer1, LOW);
     digitalWrite(buzzer2, LOW);
 
   }
