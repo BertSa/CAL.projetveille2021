e81b8f5bb21899289ea0a83170ae1a0d62ba039f
Samuel Bertrand <bertsa.pro@gmail.com>
2021-09-13 04:10:32 +0000

    clean up

--- raspberryPi/__main__.py
+++ raspberryPi/__main__.py
@@ -1,26 +1,23 @@
-import time
 import json
+import time
+
 import rpi_rf_receive
 
 
 def main():
     f = open("config.json", "r")
     data = json.load(f)
-    print(data)
-
-    print(data['ddd'])
-
     rfdevice = rpi_rf_receive.get_rf_device()
     rfdevice.enable_rx()
     timestamp = None
     while True:
         if rfdevice.rx_code_timestamp != timestamp:
             timestamp = rfdevice.rx_code_timestamp
-            if rfdevice.rx_code == 5592334:
+            if rfdevice.rx_code == data['rf_code']:
                 print("Signal received")
         time.sleep(0.01)
     # rfdevice.cleanup()
-    f.close
+    # f.close
 
 
 if __name__ == "__main__":
