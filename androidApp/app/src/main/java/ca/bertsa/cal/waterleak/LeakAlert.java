50e1ef9c2e88847b4cfb7efa54344cec0f1d2b69
Samuel Bertrand <bertsa.pro@gmail.com>
2021-09-19 21:26:08 -0400

    Creation activity LeakAlert

---
+++ androidApp/app/src/main/java/ca/bertsa/cal/waterleak/LeakAlert.java
@@ -0,0 +1,14 @@
+package ca.bertsa.cal.waterleak;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LeakAlert extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_leak_alert);
+    }
+}
