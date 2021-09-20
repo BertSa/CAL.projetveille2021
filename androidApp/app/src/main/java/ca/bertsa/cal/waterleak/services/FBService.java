baec6c311a4405adcf62432c94546f98d0b03a0d
Samuel Bertrand <bertsa.pro@gmail.com>
2021-09-19 20:42:58 -0400

    Notification working by itself

--- androidApp/app/src/main/java/ca/bertsa/cal/waterleak/services/FBService.java
+++ androidApp/app/src/main/java/ca/bertsa/cal/waterleak/services/FBService.java
@@ -1,28 +1,11 @@
 package ca.bertsa.cal.waterleak.services;
 
-import android.app.Notification;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
-import android.content.Intent;
-import android.media.RingtoneManager;
-import android.net.Uri;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
-import androidx.core.app.NotificationCompat;
-import androidx.work.Data;
-import androidx.work.OneTimeWorkRequest;
-import androidx.work.WorkManager;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
-
-import java.util.Objects;
-
-import ca.bertsa.cal.waterleak.MainActivity;
-import ca.bertsa.cal.waterleak.R;
 
 public class FBService extends FirebaseMessagingService {
     private static final String TAG = "FBService";
@@ -34,26 +17,6 @@ public class FBService extends FirebaseMessagingService {
      */
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        Log.d(TAG, "From: " + remoteMessage.getFrom());
-
-        if (remoteMessage.getData().size() > 0) {
-            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
-
-            //noinspection ConstantConditions
-            if (false) {
-                scheduleJob();
-            } else {
-                handleNow(remoteMessage);
-            }
-
-        }
-
-        if (remoteMessage.getNotification() != null) {
-            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
-//            handleNow(remoteMessage);
-            
-        }
-
     }
 
     /**
@@ -71,22 +34,6 @@ public class FBService extends FirebaseMessagingService {
         sendRegistrationToServer(token);
     }
 
-    /**
-     * Schedule async work using WorkManager.
-     */
-    private void scheduleJob() {
-        OneTimeWorkRequest work = new OneTimeWorkRequest.Builder(MyWorker.class).build();
-        WorkManager.getInstance(this).beginWith(work).enqueue();
-    }
-
-    /**
-     * Handle time allotted to BroadcastReceivers.
-     *
-     * @param remoteMessage remoteMessage
-     */
-    private void handleNow(@NonNull RemoteMessage remoteMessage) {
-        sendNotification(Objects.requireNonNull(remoteMessage.getNotification()).getBody());
-    }
 
     /**
      * Persist token to third-party servers.
@@ -98,41 +45,5 @@ public class FBService extends FirebaseMessagingService {
      */
     private void sendRegistrationToServer(String token) {
         // TODO: Implement this method to send token to your app server.
-    }
-
-    /**
-     * Create and show a simple notification containing the received FCM message.
-     *
-     * @param messageBody FCM message body received.
-     */
-    private void sendNotification(String messageBody) {
-        Log.d(TAG, "test");
-        Intent intent = new Intent(this, MainActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,
-                PendingIntent.FLAG_IMMUTABLE);
-
-        String channelId = getString(R.string.default_notification_channel_id);
-        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-        NotificationCompat.Builder notificationBuilder =
-                new NotificationCompat.Builder(this, channelId)
-                        .setSmallIcon(R.drawable.ic_stat_notification)
-                        .setContentTitle(getString(R.string.fcm_message))
-                        .setContentText(messageBody)
-                        .setCategory(Notification.CATEGORY_ALARM)
-                        .setAutoCancel(true)
-                        .setSound(defaultSoundUri)
-                        .setGroup("WaterLeaks")
-                        .setContentIntent(pendingIntent);
-
-        NotificationManager notificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-
-        NotificationChannel channel = new NotificationChannel(channelId,
-                "Channel human readable title",
-                NotificationManager.IMPORTANCE_DEFAULT);
-        notificationManager.createNotificationChannel(channel);
-
-        notificationManager.notify(0, notificationBuilder.build());
     }
 }
