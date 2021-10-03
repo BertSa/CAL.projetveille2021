1561fede741a5038ff273aab3e2a426a6d76d43c
Samuel Bertrand <bertsa.pro@gmail.com>
2021-10-03 11:42:34 -0400

    backend - listener

--- raspberryPi/bdListener.py
+++ raspberryPi/bdListener.py
@@ -2,12 +2,17 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 
+
+def listener(event):
+    print(event.event_type)  # can be 'put' or 'patch'
+    print(event.path)  # relative to the reference, it seems
+    print(event.data)  # new data at /reference/event.path. None if deleted
+
+
 cred = credentials.Certificate("GOOGLE_APPLICATION_CREDENTIALS.json")
 firebase_admin.initialize_app(cred, {
     'databaseURL': '',
 })
 
 # As an admin, the app has access to read and write all data, regradless of Security Rules
-ref = db.reference('waterleak/valve')
-print(ref.get())
-
+ref = db.reference('waterleak/valve').listen(listener)
