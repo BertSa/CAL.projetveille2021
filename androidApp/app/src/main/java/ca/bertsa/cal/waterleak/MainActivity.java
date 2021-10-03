7285c85ffe9c72c2d3f9d070a3d7ad07dd829a16
Samuel Bertrand <bertsa.pro@gmail.com>
2021-10-03 11:07:11 -0400

    Android - Cleaned

--- androidApp/app/src/main/java/ca/bertsa/cal/waterleak/MainActivity.java
+++ androidApp/app/src/main/java/ca/bertsa/cal/waterleak/MainActivity.java
@@ -3,18 +3,9 @@ package ca.bertsa.cal.waterleak;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.SwitchCompat;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.messaging.FirebaseMessaging;
 
 public class MainActivity extends AppCompatActivity {
