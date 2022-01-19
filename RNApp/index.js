e0b19012bd755fa31607006a0dbb2499b3599375
Samuel Bertrand <bertsa.pro@gmail.com>
2022-01-18 20:40:01 -0500

    Cleaning

--- RNApp/index.js
+++ RNApp/index.js
@@ -5,7 +5,7 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import myFirebase from "./mFirebase";
+import myFirebase from "./core/mFirebase";
 import PushNotification, {Importance} from "react-native-push-notification";
 
 myFirebase.initialize()
