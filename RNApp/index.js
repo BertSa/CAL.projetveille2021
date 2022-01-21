a1ceb92de45949815e95581977588726efe5da52
Samuel Bertrand <bertsa.pro@gmail.com>
2022-01-21 18:22:55 -0500

    Cleaning

--- RNApp/index.js
+++ RNApp/index.js
@@ -5,7 +5,7 @@
 import { AppRegistry } from "react-native";
 import App from "./App";
 import { name as appName } from "./app.json";
-import myFirebase from "./core/mFirebase";
+import initializeApp from "./core/AppInitializer";
 
-myFirebase.initialize();
+initializeApp();
 AppRegistry.registerComponent(appName, () => App);
